f027322cf612a88ad70c210d7deec35fcf8482ba
Jessica Wynn <Jessicashinjo@gmail.com>
2016-09-12 14:49:40 -0500

    set up test file for sensors and added plant_info to installed apps

---
+++ iris_api/plant_info/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlantInfoConfig(AppConfig):
+    name = 'plant_info'
---
+++ iris_api/plant_info/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import MoistureSensor, TemperatureSensor, LightSensor
+
+class MoistureSensorMethodTests(TestCase):
+    pass
+
+class TemperatureSensorMethodTests(TestCase):
+    pass
+
+class LightSensorMethodTests(TestCase):
+    pass
