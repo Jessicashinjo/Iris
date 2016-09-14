748b780d7e9e3fa46b77ea76008ef68b66503c41
Jessica Wynn <Jessicashinjo@gmail.com>
2016-09-14 15:58:41 -0500

    updated ulitity function to accurately show readings from all time. Changed plus sign to minus sign in tests for accurate date

--- iris_api/plant_info/utility.py
+++ iris_api/plant_info/utility.py
@@ -4,16 +4,20 @@ from django.utils import timezone
 class UtilityFunctions:
     @staticmethod
     def was_published_within_one_day():
+        published_date = timezone.now()
         return published_date >= timezone.now() - datetime.timedelta(days=1)
 
     @staticmethod
     def was_published_within_one_week():
+        published_date = timezone.now()
         return published_date >= timezone.now() - datetime.timedelta(days=7)
 
     @staticmethod
     def was_published_within_one_month():
+        published_date = timezone.now()
         return published_date >= timezone.now() - datetime.timedelta(days=30)
 
     @staticmethod
     def was_published_within_all_time():
-        return published_date >= timezone.now() - datetime.timedelta.max()
+        published_date = timezone.now()
+        return published_date > timezone.now()
