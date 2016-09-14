a530a572c9b054813a6e54672df0a14fa763034a
Jessica Wynn <Jessicashinjo@gmail.com>
2016-09-14 15:28:55 -0500

    added string function to each class in models

--- iris_api/plant_info/models.py
+++ iris_api/plant_info/models.py
@@ -1,19 +1,29 @@
 from django.db import models
+from .utility import UtilityFunctions
 
-class MoistureSensor(models.Model):
+class MoistureSensor(models.Model, UtilityFunctions):
     device_id = models.CharField(max_length=100)
     event_type = models.CharField(max_length=50)
     published_date = models.DateTimeField()
     moisture_value = models.CharField(max_length=50)
 
-class TemperatureSensor(models.Model):
+    def __str__(self):
+        return '{}: {}'.format(self.event_type, self.moisture_value)
+
+class TemperatureSensor(models.Model, UtilityFunctions):
     device_id = models.CharField(max_length=100)
     event_type = models.CharField(max_length=50)
     published_date = models.DateTimeField()
     temperature_value = models.CharField(max_length=50)
 
-class LightSensor(models.Model):
+    def __str__(self):
+        return '{}: {}'.format(self.event_type, self.temperature_value)
+
+class LightSensor(models.Model, UtilityFunctions):
     device_id = models.CharField(max_length=100)
     event_type = models.CharField(max_length=50)
     published_date = models.DateTimeField()
     light_value = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}: {}'.format(self.event_type, self.light_value)
