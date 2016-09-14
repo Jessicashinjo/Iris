112f7f603b9d52989587e89a7aa769bd8c123ee4
Jessica Wynn <Jessicashinjo@gmail.com>
2016-09-14 14:01:02 -0500

    initial model set up for sensors

--- iris_api/plant_info/models.py
+++ iris_api/plant_info/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class MoistureSensor(models.Model):
+    device_id = models.CharField(max_length=100)
+    event_type = models.CharField(max_length=50)
+    published_date = models.DateTimeField()
+    moisture_value = models.CharField(max_length=50)
+
+class TemperatureSensor(models.Model):
+    device_id = models.CharField(max_length=100)
+    event_type = models.CharField(max_length=50)
+    published_date = models.DateTimeField()
+    temperature_value = models.CharField(max_length=50)
+
+class LightSensor(models.Model):
+    device_id = models.CharField(max_length=100)
+    event_type = models.CharField(max_length=50)
+    published_date = models.DateTimeField()
+    light_value = models.CharField(max_length=50)
