5deaf9620caeaf3eead66055f9a6a43f7cdf5ea9
Jessica Wynn <Jessicashinjo@gmail.com>
2016-09-17 18:33:54 -0500

    views updated to allow user to search for information from a specific date

--- iris_api/plant_info/views.py
+++ iris_api/plant_info/views.py
@@ -16,23 +16,32 @@ class MoistureSensorView(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = MoistureSensor.objects.all()
-        since_date = self.request.query_params.get('current_date', None)
+        since_date = self.request.query_params.get('since_day', None)
         if since_date is not None:
             compare_date = timezone.now() - timedelta(days=int(since_date))
             queryset = queryset.filter(published_date__gt=compare_date)
         return queryset
-            # MoistureSensor.objects.order_by('-published_date').filter(
-            #     published_date__gte=timezone.now()
-            # )
-
-    # query params since = date
 
 class TemperatureSensorView(viewsets.ModelViewSet):
     model = TemperatureSensor
     serializer_class = TemperatureSensorSerializer
-    queryset = TemperatureSensor.objects.all()
+
+    def get_queryset(self):
+        queryset = TemperatureSensor.objects.all()
+        since_date = self.request.query_params.get('since_day', None)
+        if since_date is not None:
+            compare_date = timezone.now() - timedelta(days=int(since_date))
+            queryset = queryset.filter(published_date__gt=compare_date)
+        return queryset
 
 class LightSensorView(viewsets.ModelViewSet):
     model = LightSensor
     serializer_class = LightSensorSerializer
-    queryset = LightSensor.objects.all()
+
+    def get_queryset(self):
+        queryset = LightSensor.objects.all()
+        since_date = self.request.query_params.get('since_day', None)
+        if since_date is not None:
+            compare_date = timezone.now() - timedelta(days=int(since_date))
+            queryset = queryset.filter(published_date__gt=compare_date)
+        return queryset
