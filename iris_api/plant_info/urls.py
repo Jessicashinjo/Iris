ea0f3360d467e13478840230f916df47149c13e1
Jessica Wynn <Jessicashinjo@gmail.com>
2016-09-17 17:53:19 -0500

    added base_name to fix get_queryset error

--- iris_api/plant_info/urls.py
+++ iris_api/plant_info/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from plant_info import views
 
 router = routers.DefaultRouter()
-router.register(r'moisture', views.MoistureSensorView)
+router.register(r'moisture', views.MoistureSensorView, base_name='moisture-list')
 router.register(r'temperature', views.TemperatureSensorView)
 router.register(r'light', views.LightSensorView)
 
